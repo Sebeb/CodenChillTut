031f56620e28cdff444edad2b71955d969508004
Sebastian Parfitt <sebastian@shytea.games>
2018-11-09 14:40:09 +0100

    Initial commit

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+	public GameObject[] enemies;
+	public float minCooldown, maxCooldown, minYSpawn,maxYSpawn,xSpawn;
+	public static Game controller;
+	public int lives = 5;
+
+	void Awake()
+	{
+		controller = this;
+	}
+	
+	void Start () {
+		StartCoroutine(SpawnEnemy());
+	}
+
+	private IEnumerator SpawnEnemy()
+	{
+		Instantiate(enemies[Random.Range(0,enemies.Length)], new Vector3(xSpawn * (Random.Range(0, 2) == 1 ? 1 : -1), Random.Range(minYSpawn, maxYSpawn)),Quaternion.identity);
+		yield return new WaitForSeconds(Random.Range(minCooldown,maxCooldown));
+		StartCoroutine(SpawnEnemy());
+	}
+}
---
+++ Assets/Scripts/InitialVelocity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialVelocity : MonoBehaviour
+{
+
+	public float startVelMin, startVelMax = 99;
+	
+	void Start ()
+	{
+		Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+		//rb2d.velocity = Vector2.left * Random.Range(startVelMin,startVelMax) * Mathf.Sign(transform.position.x);
+		rb2d.velocity = new Vector2(5,0);
+		GetComponent<SpriteRenderer>().flipX = transform.position.x > 0;
+	}
+	
+}
